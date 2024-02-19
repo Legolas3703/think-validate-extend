4cf8779002946f0fbe7db0a2ecdbac635539e3de
legolas <13478993703@163.com>
2024-02-19 10:11:18 +0800

    新增验证规则

--- src/Rules.php
+++ src/Rules.php
@@ -61,30 +61,163 @@ class Rules
 
 
 	/**
-	 *
+	 * 验证是否为有效的JSON
+	 * @access public
+     * @param Srring $value 待检测的字符串
+	 * @return Bool
 	 */
-	public function isJson() : Bool
+	public function isJson(string $value) : Bool
 	{
-		return false;
+		$json = json_decode($value, true);
+		return (is_array($json) && $json) ? true : false;
 	}
 
 
 	/**
-	 *
-	 */
-	public function isJson2() : Bool
+	* 当A字段的值在列表中时，B字段为必填
+	* @access public
+	* @param Mixed $value B字段值
+	* @param String $rule 验证规则 格式：字段A的名字,列表值1,列表值2...列表值n
+	* @param Array $data 数据
+	* @return Bool
+	*/
+	public function requireIn($value, string $rule, array $data=[]) : Bool
 	{
-		return false;
+		$rule_arr = explode(',', $rule);
+		$field = array_shift($rule_arr);
+		if( in_array($data[$field], $rule_arr) )
+		{
+			return empty($value) ? false : true;
+		}
+		return true;
+	}
+
+	/**
+	* 验证值是否存在于指定表字段中
+	* @access public
+	* @param String $value 字段值
+	* @param String $rule 验证规则 格式：数据表名（不含前缀）,字段名
+	* @return Bool
+	*/
+	public function inTable(string $value, string $rule) : Bool
+	{
+		$rule_arr = explode(',', $rule);
+		$table = $rule_arr[0];
+		$field = $rule_arr[1];
+
+		$db = app()->db->name($table);
+		$check = $db->where($field, $value)->field('id')->find();
+
+		return $check ? true : false;
 	}
 
 
 	/**
-	 *
-	 */
-	public function isJson3() : Bool
+	* 验证值是否不存在于指定表字段中
+	* @access public
+	* @param String $value 字段值
+	* @param String $rule 验证规则 格式：数据表,字段名
+	* @return Bool
+	*/
+	public function notInTable(string $value, string $rule) : Bool
 	{
-		// return true;
-		return false;
+		$check = $this->inTable($value, $rule);
+		return $check ? false : true;
+	}
+
+
+	/**
+	* 验证字符串字节数是否达到最小值
+	* @access public
+	* @param String $value 字段值
+	* @param Int $rule 验证规则 格式：字节数
+	* @return Bool
+	*/
+	public function minByte(string $value, int $rule) : Bool
+	{
+		$length = strlen($value);
+
+		return $length >= $rule;
+	}
+
+
+	/**
+	* 验证字符串字节数是否超过最大值
+	* @access public
+	* @param String $value 字段值
+	* @param Int $rule 验证规则 格式：字节数
+	* @return Bool
+	*/
+	public function maxByte(string $value, int $rule) : Bool
+	{
+		$length = strlen($value);
+
+		return $length <= $rule;
+	}
+
+
+	/**
+	* 验证字符串字节数，支持定长/区间验证
+	* @access public
+	* @param String $value 字段值
+	* @param String $rule
+	*	定长验证规则 格式：字节数（整数）
+	*	区间验证规则 格式：最小值（整数）,最大值（整数）
+	* @return Bool
+	*/
+	public function lengthByte(string $value, string $rule) : Bool
+	{
+		$length = strlen($value);
+
+		// 区间验证
+		if( strpos($rule, ',') )
+		{
+			// 长度区间
+			list($min, $max) = explode(',', $rule);
+			return ($length >= $min) && ($length <= $max);
+		}
+
+		// 指定长度
+		return $length == $rule;
+	}
+
+
+	/**
+	 * 过滤数组
+	 * @access public
+	 * @param Array $value 字段值
+	 * @param String $rule 验证规则 [normal-普通模式，有非空值即可通过 strict-严格模式，禁止存在空值]
+	 * @return Bool
+	 */
+	public function arrayFilter(array $value, string $rule) : Bool
+	{
+		switch($rule)
+		{
+			// 普通模式
+			case 'normal':
+				foreach($value as $key=>$val)
+				{
+					if( !empty($val) )
+					{
+						return true;
+					}
+				}
+				return false;
+			// 严格模式
+			case 'strict':
+				foreach($value as $key=>$val)
+				{
+					if( empty($val) )
+					{
+						unset($value[$key]);
+						continue;
+					}
+				}
+				return empty($value) ? false : true;
+			// 未定义的模式，一律返回false
+			default:
+				return false;
+		}
 	}
 
 
